c806edbff7acc19c42c662c7c137f4e89319401b
JeanIfsp <jean.marciel@aluno.ifsp.edu.br>
2023-12-13 09:39:08 -0300

    Created list to funcionarios

--- src/app/funcionario/funcionario-list/funcionario-list.component.ts
+++ src/app/funcionario/funcionario-list/funcionario-list.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { AuthService } from 'src/app/security/auth.service';
+import { FuncionarioService } from '../funcionario.service';
+import { Funcionario } from 'src/app/core/model';
 
 @Component({
   selector: 'app-funcionario-list',
   templateUrl: './funcionario-list.component.html',
   styleUrls: ['./funcionario-list.component.css']
 })
-export class FuncionarioListComponent {
 
+export class FuncionarioListComponent {
+  header = "Posto-Ui-Funcionario"
+
+  funcionarios: Funcionario[] = [];
+
+  constructor(private funcionarioService:FuncionarioService,
+    private confirmation: ConfirmationService,
+    private messageService: MessageService,
+    private errorHandler: ErrorHandlerService,
+    private title: Title,
+    public auth: AuthService){ }
+
+    ngOnInit(): void {
+      this.title.setTitle('Listagem de Funcionários');
+      this.list();
+    }
+  
+    list(): void {
+      this.funcionarioService.listFuncionario()
+        .then(result => {
+          this.funcionarios = result;
+          console.log("Funcionarios: ",this.funcionarios)
+        })
+        .catch(error => this.errorHandler.handle(error));
+    }
+  
 }
