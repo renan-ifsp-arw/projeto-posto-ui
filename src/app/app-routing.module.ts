e56bdff30a8782ede4e7c4ec7b959b84e4920686
braga-apixio <rbraga@apixio.com>
2023-12-13 23:25:32 -0300

    mais changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,51 +1,53 @@
-import { ClienteRegisterComponent } from './clientes/cliente-register/cliente-register.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ClientesListComponent } from './clientes/clientes-list/clientes-list.component';
-import { LoginFormComponent } from './security/login-form/login-form.component';
-import { UserRegisterComponent } from './users/user-register/user-register.component';
-import { FuncionarioRegisterComponent } from './funcionario/funcionario-register/funcionario-register.component';
-import { BombaRegisterComponent } from './bomba/bomba-register/bomba-register.component';
-import { PageNotFoundComponent } from './core/page-not-found.component';
-import { NotAuthorizedComponent } from './core/not-authorized.component';
-import { AuthGuard } from './security/auth.guard';
+import {ClienteRegisterComponent} from './cliente/cliente-register/cliente-register.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ClientesListComponent} from './cliente/cliente-list/clientes-list.component';
+import {LoginFormComponent} from './security/login-form/login-form.component';
+import {UserRegisterComponent} from './users/user-register/user-register.component';
+import {FuncionarioRegisterComponent} from './funcionario/funcionario-register/funcionario-register.component';
+import {BombaRegisterComponent} from './bomba/bomba-register/bomba-register.component';
+import {PageNotFoundComponent} from './core/page-not-found.component';
+import {NotAuthorizedComponent} from './core/not-authorized.component';
+import {AuthGuard} from './security/auth.guard';
 
 const routes: Routes = [
 
   //{ path: '', redirectTo: '', pathMatch: 'full' },
   {
-    path: 'clientes/:id',
+    path: 'cliente/:id',
     component: ClienteRegisterComponent,
     canActivate: [AuthGuard],
-    data: { roles: ['ROLE_REGISTER_ACTIVITY']}
+    data: {roles: ['ROLE_ABASTECIMENTO', 'ROLE_CONVENIENCIA', 'ROLE_GERENCIA']}
   },
-  { path: 'clientes',
+  {
+    path: 'cliente',
     component: ClientesListComponent,
     canActivate: [AuthGuard],
-    data: { roles: ['ROLE_SEARCH_ACTIVITY']}
+    data: {roles: ['ROLE_ABASTECIMENTO', 'ROLE_CONVENIENCIA', 'ROLE_GERENCIA']}
   },
   {
-    path: 'clientes/new',
+    path: 'cliente/new',
     component: ClienteRegisterComponent,
     canActivate: [AuthGuard],
-    data: { roles: ['ROLE_REGISTER_ACTIVITY']}
+    data: {roles: ['ROLE_ABASTECIMENTO', 'ROLE_CONVENIENCIA', 'ROLE_GERENCIA']}
   },
   {
-     path: 'users/new',
-     component: UserRegisterComponent,
-     
-     },
-  
-  { path: 'login', component: LoginFormComponent },
-  { path: 'funcionario', component: FuncionarioRegisterComponent },
-  { path: 'bomba', component: BombaRegisterComponent },
-  { path: 'page-not-found', component: PageNotFoundComponent },
-  { path: 'not-authorized', component: NotAuthorizedComponent }, // pagina-nao-encontrada
-  { path: '**', redirectTo: 'page-not-found'} // importante que seja a última rota
+    path: 'users/new',
+    component: UserRegisterComponent,
+
+  },
+
+  {path: 'login', component: LoginFormComponent},
+  {path: 'funcionario', component: FuncionarioRegisterComponent},
+  {path: 'bomba', component: BombaRegisterComponent},
+  {path: 'page-not-found', component: PageNotFoundComponent},
+  {path: 'not-authorized', component: NotAuthorizedComponent}, // pagina-nao-encontrada
+  {path: '**', redirectTo: 'page-not-found'} // importante que seja a última rota
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,28 @@ import { AuthService } from './security/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClientesModule } from './clientes/clientes.module';
+import { ClientesModule } from './cliente/clientes.module';
 import { SharedModule } from './shared/shared.module';
 import { UsersModule } from './users/users.module';
 import { FuncionarioModule } from './funcionario/funcionario.module';
 import { BombaModule } from './bomba/bomba.module';
+import { VendaConvenienciaRegisterComponent } from './venda-conveniencia/venda-conveniencia-register/venda-conveniencia-register.component';
+import { VendaConvenienciaListComponent } from './venda-conveniencia/venda-conveniencia-list/venda-conveniencia-list.component';
+import { AbastecimentoListComponent } from './abastecimento/abastecimento-list/abastecimento-list.component';
+import { AbastecimentoRegisterComponent } from './abastecimento/abastecimento-register/abastecimento-register.component';
+import { OrdemCompraRegisterComponent } from './ordem-compra/ordem-compra-register/ordem-compra-register.component';
+import { OrdemCompraListComponent } from './ordem-compra/ordem-compra-list/ordem-compra-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    VendaConvenienciaRegisterComponent,
+    VendaConvenienciaListComponent,
+    AbastecimentoListComponent,
+    AbastecimentoRegisterComponent,
+    OrdemCompraRegisterComponent,
+    OrdemCompraListComponent,
   ],
   imports: [
     BrowserModule,
