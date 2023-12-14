f883d48c44e7f2ad89f40d6bf0ccc079404d07ca
braga-apixio <rbraga@apixio.com>
2023-12-13 23:27:34 -0300

    add comps

---
+++ src/app/abastecimento/abastecimento-list/abastecimento-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abastecimento-list',
+  templateUrl: './abastecimento-list.component.html',
+  styleUrls: ['./abastecimento-list.component.css']
+})
+export class AbastecimentoListComponent {
+
+}
---
+++ src/app/venda-conveniencia/venda-conveniencia-register/venda-conveniencia-register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-venda-conveniencia-register',
+  templateUrl: './venda-conveniencia-register.component.html',
+  styleUrls: ['./venda-conveniencia-register.component.css']
+})
+export class VendaConvenienciaRegisterComponent {
+
+}
